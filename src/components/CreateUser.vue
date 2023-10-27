f4e252c6e2c27f5794c79f8f7b8921f1d78fab82
Ugz31 <occelli.ugo@gmail.com>
2023-10-27 17:54:41 +0200

    Add : CSS / Component : CreateUser.vue

--- src/components/CreateUser.vue
+++ src/components/CreateUser.vue
@@ -1,38 +1,66 @@
 <template>
-  <div>
-    <h2>Ajouter un Nouvelle Utilisateur</h2>
-    <!-- preventDefault pour ne pas propager l'evenement et recharger la page -->
-    <form @submit.prevent="createUser">
-      <div>
-        <label for="email">Email : </label>
-        <input v-model="user.email" type="email" required />
+  <div class="container">
+    <section class="section">
+      <div class="columns is-centered">
+        <div class="column is-half">
+          <h2 class="title" style="text-decoration: underline">Ajouter un Nouvelle Utilisateur</h2>
+          <div style="height: 1px; background-color: gray; margin-bottom: 10px"></div>
+          <!-- preventDefault pour ne pas propager l'evenement et recharger la page -->
+          <form @submit.prevent="createUser">
+            <div class="field">
+              <label class="label" for="email">Email : </label>
+              <div class="control">
+                <input class="input" v-model="user.email" type="email" required />
+              </div>
+            </div>
+
+            <div class="field">
+              <label class="label">Nom :</label>
+              <div class="control">
+                <input class="input" v-model="user.name" type="text" required />
+              </div>
+            </div>
+
+            <div class="field">
+              <label class="label">Prénom :</label>
+              <div class="control">
+                <input class="input" v-model="user.firstname" type="text" required />
+              </div>
+            </div>
+            <div style="height: 1px; background-color: gray; margin-bottom: 10px"></div>
+            <div class="field">
+              <label class="label">Adresse</label>
+              <div class="control">
+                <input style="margin-bottom: 15px" class="input" v-model="user.address.line" type="text" required placeholder="123 rue..." />
+              </div>
+              <label class="label">Code postal</label>
+              <div class="control">
+                <input style="margin-bottom: 15px" class="input" v-model="user.address.zipcode" type="text" placeholder="12345" />
+              </div>
+              <label class="label">Ville</label>
+              <div class="control">
+                <input style="margin-bottom: 15px" class="input" v-model="user.address.city" type="text" required placeholder="Toulouse" />
+              </div>
+              <label class="label">Pays</label>
+              <div class="control">
+                <input style="margin-bottom: 15px" class="input" v-model="user.address.country" type="text" placeholder="France" />
+              </div>
+            </div>
+            <div>
+              <label class="label">Mot de passe</label>
+              <div class="control">
+                <input style="margin-bottom: 15px" class="input" v-model="user.password" type="text" />
+              </div>
+            </div>
+            <div style="height: 1px; background-color: gray; margin-bottom: 10px"></div>
+            <div class="control">
+              <button class="button is-link" type="submit">Envoyé</button>
+            </div>
+          </form>
+          <div v-if="errorMessage" class="notification is-danger">{{ errorMessage }}</div>
+        </div>
       </div>
-      <div>
-        <label>Nom :</label>
-        <input v-model="user.name" type="text" required />
-      </div>
-      <div>
-        <label>Prénom :</label>
-        <input v-model="user.firstname" type="text" required />
-      </div>
-      <div>
-        <h4>Adresse :</h4>
-        <label>Adresse</label>
-        <input v-model="user.address.line" type="text" required placeholder="123 rue..." />
-        <label>Code postal</label>
-        <input v-model="user.address.zipcode" type="text" placeholder="12345" />
-        <label>Ville</label>
-        <input v-model="user.address.city" type="text" required placeholder="Toulouse" />
-        <label>Pays</label>
-        <input v-model="user.address.country" type="text" placeholder="France" />
-      </div>
-      <div>
-        <label>Mot de passe</label>
-        <input v-model="user.password" type="text" />
-      </div>
-      <button type="submit">Envoyé</button>
-    </form>
-    <div v-if="errorMessage" class="notification is-danger">{{ errorMessage }}</div>
+    </section>
   </div>
 </template>
 
